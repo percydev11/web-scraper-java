4aaef6d7c9e1403e6269e8b94fc1461be2240d31
percydev11 <percydeveloper@gmail.com>
2023-03-31 05:49:43 -0500

    Update pom.xml and application properties (data base)

--- src/main/java/com/webscraperjava/webscraper/controllers/SearchController.java
+++ src/main/java/com/webscraperjava/webscraper/controllers/SearchController.java
@@ -1,9 +1,11 @@
 package com.webscraperjava.webscraper.controllers;
 
+import com.webscraperjava.webscraper.entities.WebPage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,6 +14,9 @@ public class SearchController {
     @RequestMapping(value = "api/search", method = RequestMethod.GET)
     public List<WebPage> search() {
         List<WebPage> result = new ArrayList<>();
-
+        WebPage myPage = new WebPage();
+        myPage.setTitle("Test");
+        myPage.setDescription("My test page");
+        return result;
     }
 }
--- src/main/java/com/webscraperjava/webscraper/entities/WebPage.java
+++ src/main/java/com/webscraperjava/webscraper/entities/WebPage.java
@@ -1,11 +1,31 @@
 package com.webscraperjava.webscraper.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Table(name = "webpage")
+@Getter @Setter
+@ToString @EqualsAndHashCode
 public class WebPage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+    @Column(name = "url")
     private String url;
+
+    @Column(name = "tittle")
     private String title;
+
+
+    @Column(name = "description")
     private String description;
 }
 
